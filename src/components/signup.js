139766661faff774887d6c78240119fb2d5cccd8
Bhavna Sharma <bhavna@i2k2.com>
2025-07-17 16:22:11 +0530

    integrate api

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,32 +1,32 @@
-import {react, useState} from 'react';
-
-const SignUp =()=>{
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const collectData =()=>{console.log(name,email,password);
+import { react, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+const SignUp = () => {
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const collectData = async () => {
+        console.log(name, email, password);
+        let result = await fetch('http://localhost:5600/register', {
+            method: 'post',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        result = await result.json();
+        console.log(result)
+        if (result) {
+            navigate('/');
+        }
     }
-    return(
+    return (
         <div className='register'>
             <h3>Register</h3>
-            <input className='inputBox' type="text" placeholder='Enter Name' onChange={(e)=>setName(e.target.value)}/>
-            <input className='inputBox' type="email" placeholder='Enter Email' onChange={(e)=>setEmail(e.target.value)}/>
-            <input className='inputBox' type="password" placeholder='Enter Password' onChange={(e)=>setPassword(e.target.value)}/>
+            <input className='inputBox' type="text" placeholder='Enter Name' onChange={(e) => setName(e.target.value)} />
+            <input className='inputBox' type="email" placeholder='Enter Email' onChange={(e) => setEmail(e.target.value)} />
+            <input className='inputBox' type="password" placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} />
             <button type="button" className='button' onClick={collectData}>SIGN UP</button>
         </div>
     )
 }
 
 export default SignUp;
-
-
-
-// require('./db/config');
-// const userModel = require('./db/user')
-// const express = require('express');
-// const app = express();
-
-// app.post("/register",(req,resp)=>{
-//   resp.send("Connected");
-// })
-// app.listen(5600);
