f9cff9c1e7b9f14b07f4d7c36428e12b5c66d63d
Bhavna Sharma <bhavna@i2k2.com>
2025-07-18 19:28:42 +0530

    set logo at navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../react-logo.png';  // Import logo.svg from src folder
 import { Link, useNavigate } from 'react-router-dom'
 
 const Nav = () => {
@@ -11,15 +12,19 @@ const Nav = () => {
     }
     return (
         <div>
+            <img src={logo} alt="logo" className='logo'/>
+            {auth ?
             <ul className="nav-ul">
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                {
-                    auth ? <li><Link to="/signup" onClick={logOut}>Logout</Link></li> : <><li><Link to="/login">Login</Link></li><li><Link to="/login">Login</Link></li></>
-                }
+                <li><Link to="/signup" onClick={logOut}>Logout ({JSON.parse(auth).name})</Link></li>
+            </ul> :
+            <ul className="nav-ul textRight">
+                <li><Link to="/signup">Sign Up</Link></li><li><Link to="/login">Login</Link></li>
             </ul>
+        }
         </div>
     )
 }
