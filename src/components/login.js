ed4b38da6ea150f48b4f6d1643402826e49f1eb3
Bhavna Sharma <bhavna@i2k2.com>
2025-07-18 18:41:03 +0530

    updated code

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
+    const navigate = useNavigate();
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
-    const loginClick = () => {
+
+    useEffect(()=>{
+        const auth = localStorage.getItem('user');
+        if(auth){
+            navigate("/");
+        }
+    })
+    const loginClick = async () => {
         console.log(name, password)
+        let result = await fetch("http://localhost:5600/login", {
+            method: 'post',
+            body: JSON.stringify({ name, password }),
+            headers: { 'Content-type': 'application/json' }
+        });
+        result = await result.json();
+        localStorage.setItem('user', JSON.stringify(result))
+        console.log(result);
+        if (result.name) {
+            navigate("/")
+        } else {
+            alert("Please Enter Valid Name")
+        }
     }
     return (
         <div className='login'><h2>Login</h2>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,8 +16,9 @@ const Nav = () => {
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li>{auth ? <Link to="/signup" onClick={logOut}>Logout</Link> : <Link to="/signup">SignUp</Link>}</li>
-                <li><Link to="/login">Login</Link></li>
+                {
+                    auth ? <li><Link to="/signup" onClick={logOut}>Logout</Link></li> : <><li><Link to="/login">Login</Link></li><li><Link to="/login">Login</Link></li></>
+                }
             </ul>
         </div>
     )
